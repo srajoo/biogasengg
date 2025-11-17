bbba817963504817128f4afc3afe78291b7f7033
srajoo <103288051+srajoo@users.noreply.github.com>
2025-11-17 11:17:52 -0800

    Add files via upload

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Page from "../components/about-us/index"
+
+
+export default function AboutPage() {
+  return (
+    <div>
+      <Page />
+    </div>
+  );
+}
